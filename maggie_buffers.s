66d861c5c4521d78170253ef5f3ec3aa2531d1e9
Morten Brenna <morten@myvr-software.com>
2023-02-26 18:14:29 +0100

    Added asm file for the clear

---
+++ maggie_buffers.s
@@ -0,0 +1,10 @@
+	section .text
+;	void magFastClear(void *buffer __asm("a0"), ULONG nBytes__asm("d0"), ULONG data __asm("d1"));
+_magFastClear:
+	vperm	#$76547654,d1,d1,e0
+.loop:
+	store	e0,(a0)+
+	subq.l	#8,d0
+	bne.s	.loop
+	rts
+	public _magFastClear
